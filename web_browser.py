ca360d557190fc14b4132bd7131cfe78427a9a48
Sumit Sahu <sumit43508@gmail.com>
2024-09-30 13:14:04 +0530

    first commit

---
+++ web_browser.py
@@ -0,0 +1,62 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+class WebBrowser(QMainWindow):
+    def __init__(self):
+        super(WebBrowser, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://www.google.com"))
+
+        # Set the central widget of the Window to the browser
+        self.setCentralWidget(self.browser)
+
+        # Create navigation bar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        # Back button
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        # Forward button
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        # Reload button
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        # Home button
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        # URL bar
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        # Update URL bar when the browser navigates
+        self.browser.urlChanged.connect(self.update_url)
+
+        self.show()
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("https://www.google.com"))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName("Sumit Browser")
+window = WebBrowser()
+app.exec_()
